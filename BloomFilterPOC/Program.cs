9c492067d7bd959405ff1b0c8312ae89d5bccc9d
Simon Wu <siw@microsoft.com>
2018-02-12 16:08:07 +0800

    update

--- BloomFilterPOC/Program.cs
+++ BloomFilterPOC/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Document.Hosting;
 using Microsoft.Document.Hosting.RestClient;
 using Microsoft.Document.Hosting.RestService.Contract;
 using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace BloomFilterPOC
 {
@@ -17,9 +19,9 @@ namespace BloomFilterPOC
         {
             string baseUri = "https://op-dhs-prod-read-nus.azurewebsites.net/";// "https://op-dhs-sandbox-read.azurewebsites.net/"; //
             string clientName = "integration_test";
-
+           
             IDocumentHostingService dhsClient = new DocumentHostingServiceClient(new Uri(baseUri), clientName, accessKey);
-
+            IList<DepotBloomFilter> result = new List<DepotBloomFilter>();
             Dictionary<string, List<string>> conflicts = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
             IList<GetDepotResponse> depots = dhsClient.GetAllDepotsBySiteBasePath("docs", "docs.microsoft.com/dotnet/", null, CancellationToken.None).Result;
             foreach (GetDepotResponse depot in depots)
@@ -29,39 +31,44 @@ namespace BloomFilterPOC
                     string depotName = depot.DepotName;
                     string continueAt = null;
                     Console.WriteLine($"{depotName} Start.");
+                    IEnumerable<GetDocumentResponse> allDocuments = new List<GetDocumentResponse>();
+                    int i = 0;
                     do
                     {
                         try
                         {
                             GetDocumentsResponse documents = dhsClient.GetDocumentsPaginated(depot.DepotName, "en-us", "live", false, continueAt, null, null, CancellationToken.None).Result;
                             continueAt = documents.ContinueAt;
-                            foreach (GetDocumentResponse document in documents.Documents)
-                            {
-                                string canonicalUrl = document.Metadata.GetValueOrDefault<string>("canonical_url");
-                                if (!string.IsNullOrEmpty(canonicalUrl))
-                                {
-                                    if (conflicts.ContainsKey(canonicalUrl))
-                                    {
-                                        conflicts[canonicalUrl].Add(depotName);
-                                    }
-                                    else
-                                    {
-                                        conflicts[canonicalUrl] = new List<string> { depotName };
-                                    }
-                                }
-                            }
+                            allDocuments = allDocuments.Concat(documents.Documents);
                         }
                         catch (Exception e)
                         {
                             Console.WriteLine(e);
                         }
-                        Console.WriteLine("\t\t..................");
+                        i++;
+                        Console.WriteLine($"{i:000} ..................");                        
                     }
                     while (!string.IsNullOrEmpty(continueAt));
-
+                    Console.WriteLine($"{depotName} Size: {allDocuments.Count()}.");
+                    var bloomFilter = new BloomFilter(allDocuments.Count(), 0.001);
+                    foreach (var document in allDocuments)
+                    {
+                        bloomFilter.Add(document.AssetId);
+                    }
+                    Console.WriteLine($"{depotName} Bloom Filter Size: {bloomFilter.BitLength / 8} KB.");
+                    result.Add(new DepotBloomFilter
+                    {
+                        DepotName = depotName,
+                        BloomFilter = bloomFilter.BitArray
+                    });
                     Console.WriteLine($"{depotName} Done.");
                 }
             }
+            using (StreamWriter file = new StreamWriter(@"output.json", true))
+            {
+                file.WriteLine(JsonConvert.SerializeObject(result));
+            }
+            Console.ReadLine();
         }
 
         public static void Exp1()
